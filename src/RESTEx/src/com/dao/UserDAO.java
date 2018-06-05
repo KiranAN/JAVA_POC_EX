0555829ef742c31ca21322028a2d5b4954b0bc55
KIRAN A N <Kiranan1988@gmail.com>
2018-06-05 12:33:51 +0530

    Update UserDAO.java

--- src/RESTEx/src/com/dao/UserDAO.java
+++ src/RESTEx/src/com/dao/UserDAO.java
@@ -7,6 +7,6 @@ public interface UserDAO {
 	public List<UserMsg> getAllUsers();
 	public UserMsg getUser(int id);
 	public int createUser(UserMsg user);
-	public void updateUser(UserMsg user);
+	public void updateUser(UserMsg user, int id);
 	public boolean deleteUser(int id);
 }
